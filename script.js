e4ca397403c7b994a910e22931e62b66f03cdeb9
Erlandito <123456789+odin@users.noreply.github.com>
2026-07-09 02:10:11 +0700

    add style to website

--- script.js
+++ script.js
@@ -44,11 +44,14 @@ function playRound(computerChoice,HumanChoice,SCORE){
 
 let SCORE = [0,0]; //[human_score, computer_score]
 let HumanChoice = "";
+let maxScore = 0;
 
+//element in html
 const putHumanChoice = document.getElementById("getChoice");
-const saveButton = document.getElementById("putChoice");
 const startButton = document.getElementById("startGame");
+const changeScoreButton = document.getElementById("changeScore");
 const resetButton = document.getElementById("scoreReset");
+const gameResultOutput = document.getElementById("gameResultOutput")
 let HumanChoiceOutput = document.getElementById("human-choice");
 let computerChoiceoutput = document.getElementById("computer-choice");
 let humanScoreoutput = document.getElementById("human-score");
@@ -57,24 +60,43 @@ let computerScoreOutput = document.getElementById("computer-score");
 humanScoreoutput.innerText = SCORE[0];
 computerScoreOutput.innerText = SCORE[1];
 
-saveButton.addEventListener("click", function(){
+startButton.addEventListener("click",function(){
     HumanChoice = Number(putHumanChoice.value);
     HumanChoice = getHumanChoice(HumanChoice);
-})
-
-startButton.addEventListener("click",function(){
     let computerChoice = getComputerChoice(Math.random())
     SCORE = playRound(computerChoice,HumanChoice,SCORE);
     computerChoiceoutput.innerText = computerChoice;
     HumanChoiceOutput.innerText = HumanChoice;
     humanScoreoutput.innerText = SCORE[0];
     computerScoreOutput.innerText = SCORE[1];
+    if (SCORE[0] === maxScore){
+        gameResultOutput.textContent = "YOU WIN!!";
+        gameResultOutput.style.display = "block";
+        startButton.style.display = "none";
+    } else if(SCORE[1] === maxScore){
+        gameResultOutput.textContent = "COMPUTER WIN!!";
+        gameResultOutput.style.display = "block";
+        startButton.style.display = "none";
+    } 
+})
+
+changeScoreButton.addEventListener("click",function(){
+    maxScore = Number(prompt("change max score (default max score is 5)"));
+    console.log(`input succes, new maxScore is ${maxScore}`);
 })
 
 resetButton.addEventListener("click", function(){
+    gameResultOutput.style.display = "none";
     SCORE = [0,0]
     humanScoreoutput.innerText = SCORE[0];
     computerScoreOutput.innerText = SCORE[1];
         console.log(`THE SCORE HAS BEEN RESET\nhuman score : ${SCORE[0]}\ncomputer score : ${SCORE[1]}`)
+    startButton.style.display = "inline";
 })
 
+resetButton.onmouseenter = () => resetButton.style.backgroundColor = "black"
+resetButton.onmouseleave = () => resetButton.style.backgroundColor = "rgb(53, 104, 87)"
+changeScoreButton.onmouseenter = () => changeScoreButton.style.backgroundColor = "black"
+changeScoreButton.onmouseleave = () => changeScoreButton.style.backgroundColor = "rgb(53, 104, 87)"
+startButton.onmouseenter = () => startButton.style.backgroundColor = "black"
+startButton.onmouseleave = () => startButton.style.backgroundColor = "rgb(53, 104, 87)"
